ec5746c9eeac39042847065482ce56eb8e5166d4
Yan Fonseca <yangoular@gmail.com>
2023-02-13 01:37:16 -0300

    Criação da entrada de dados.

--- Projeto06/Program.cs
+++ Projeto06/Program.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Projeto06
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args){
+            Funcionario f = new Funcionario();
+            Console.WriteLine("Insira o nome; ");
+            f.Nome = Console.ReadLine();
+            Console.WriteLine("Salário Bruto; ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
         }
     }
