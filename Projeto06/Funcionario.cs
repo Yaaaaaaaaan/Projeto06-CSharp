4f1389c3123ec676b4e10bf2b1e365d21ea23fbe
Yan Fonseca <yangoular@gmail.com>
2023-02-13 01:24:19 -0300

    Criação da classe Funcionario, declaração de variáveis Nome, Salário, Imposto, e métodos SalarioLiquido e AumentarSalario.

---
+++ Projeto06/Funcionario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Projeto06
+{
+    internal class Funcionario
+    {
+        public string nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido(){
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double Porcentagem){
+            SalarioBruto += (SalarioBruto * Porcentagem / 100.0); 
+        }
+    }
+}
